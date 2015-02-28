8e7516025f60d2d65d96d6fbf392a367cac19e7c
Tranp3 <phu.tran@mrjaffesclass.com>
2015-02-28 02:16:00 -0800

    PDA

---
+++ CameronCPDA/View.java
@@ -0,0 +1,80 @@
+
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+/**
+ * The class manages the main GUI.
+ * 
+ * @author (Cameron Cheever) 
+ * @version (2/11/15)
+ */
+
+@SuppressWarnings("serial")
+public class View extends JFrame implements ActionListener
+{
+
+    JLabel info;
+    JTextField input;
+    JButton go;
+    
+    public View()
+    {
+        setLayout(new FlowLayout());
+        
+        info = new JLabel("Enter your age here...");
+        info.setSize(20, 20);
+        add(info);
+        
+        input = new JTextField(20);
+        input.setEditable(true);
+        add(input);
+        
+        go = new JButton("Find matches");
+        go.setSize(20, 20);
+        go.addActionListener(this);
+        add(go);
+        
+    }
+    
+	//Will throw many errors
+	//TODO fix
+    @Override
+    public void actionPerformed(ActionEvent e) 
+    {
+        Object cause = e.getSource();
+        
+        if(cause==go)
+        {
+           double f = Double.parseDouble(input.getText());
+       
+           int foo = (int) Math.floor(f);
+           
+           if(model.getHighest(foo)==0||model.getHighest(foo)==0)
+           {
+        	   
+        	  DatingAges gui = new DatingAges("Sorry, but "+foo+" is too young to date.", "");
+        	  gui.setSize(300, 200);
+        	  gui.setDefaultCloseOperation(1);
+           	  gui.setVisible(true);
+           }
+           else
+           {
+        	   String g = "The highest age you can date is "+ model.getHighest( foo) ;
+        	   String h = "The lowest age you can date is " + model.getLowest( foo);
+            
+            	DatingAges gui = new DatingAges(g, h);
+            	gui.setSize(300, 200);
+            	gui.setDefaultCloseOperation(1);
+            	gui.setVisible(true);
+           }
+            
+        }
+    }
+    
+}
